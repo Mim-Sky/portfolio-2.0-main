4c1bb3f016ad7553f481252ab8864a02c913c046
mimsky <mimsky@debian.home.arpa>
2024-04-02 21:58:14 +0000

    Projects h1 made responsive

--- src/app/about.jsx
+++ src/app/about.jsx
@@ -2,9 +2,9 @@ import CardsContainer from "@/components/cardsContainer";
 
 const About = () => {
     return (
-        <div className="h-screen p-5 flex justify-center">
+        <div className="md:h-screen p-5 flex justify-center">
             <div className="flex flex-col md:grid md:grid-cols-[auto_1fr] gap-4 items-center container">
-                <h1 className="text-3xl lg:text-5xl xl:text-7xl font-bold md:rotate-[-90deg] origin-bottom-left md:translate-y-24 md:self-auto self-center">PROJECTS</h1>
+                <h1 className="p-5 text-3xl lg:text-5xl xl:text-7xl font-bold md:rotate-[-90deg] origin-top-left md:translate-y-24 md:self-auto self-center">PROJECTS</h1>
             
             <CardsContainer />
         
