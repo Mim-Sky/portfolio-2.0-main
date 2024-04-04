173321abad64391506e6ddd9ac65402016deeee3
Mim-Sky <mimsky@debian.home.arpa>
2024-04-04 18:21:55 +0000

    preparing cards to useState

--- src/app/about.jsx
+++ src/app/about.jsx
@@ -5,9 +5,7 @@ const About = () => {
         <div className="md:h-screen p-5 flex justify-center">
             <div className="flex flex-col md:grid md:grid-cols-[auto_1fr] gap-4 items-center container">
                 <h1 className="p-5 text-3xl lg:text-5xl xl:text-7xl font-bold md:rotate-[-90deg] origin-top-left md:translate-y-24 md:self-auto self-center">PROJECTS</h1>
-            
             <CardsContainer />
-        
         </div>
     </div>
     );
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 const Card = ({ title, image, description }) => {
+
+  const [selected, setSelected] = useState();
+
+  const clickHandler = () => {
+    
+  };
+
+
   return (
     <div 
-      className="min-h-96 transition duration-300 ease-in-out transform bg-gray-200 hover:bg-gray-100 p-4 rounded-lg shadow-lg"
+      className="flex flex-col justify-center items-center gap-3 min-h-96  transition duration-300 ease-in-out transform bg-gray-200 hover:bg-gray-100 p-4 rounded-lg shadow-lg"
       
     >
       <h2 className="text-xl font-bold text-gray-700 hover:text-black">{title}</h2>
       <img src={image} alt={title} className="w-full grayscale hover:grayscale-0" />
       <p className="text-gray-600 hover:text-black">{description}</p>
+      <button onClick={clickHandler} className='p-2 rounded text-white bg-slate-600 hover:bg-slate-500 duration-200 ease-in-out mt-6'>More defails</button>
     </div>
   );
 };
