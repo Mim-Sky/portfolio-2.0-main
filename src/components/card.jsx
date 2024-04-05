274c9045e9bd0a1823d79eb39444180e427af594
Mim-Sky <horodyski.michal@gmail.com>
2024-04-05 13:46:00 +0000

    1. Card styling finished 2. reduced motion introduced to existing framer motion elements 3. folder structure updated
    Folderstructure

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -12,31 +12,29 @@ const Card = ({ title, image, description, projectDetails }) => {
       className="min-h-96 transition duration-300 ease-in-out transform bg-gray-200 hover:bg-gray-100 p-4 rounded-lg shadow-lg"
     >
       {selected ? (
-        <div className='flex flex-col justify-between items-center gap-3 h-full  '>
+        <div className='flex flex-col'>
           <div className="flex justify-between items-center gap-2 w-full">
             <button onClick={clickHandler} className='p-2 rounded text-white bg-slate-600 hover:bg-slate-500 duration-200 ease-in-out'>Back</button>
             <a href="#" className='p-2 rounded text-white bg-slate-600 hover:bg-slate-500 duration-200 ease-in-out text-center'>Visit website</a>
           </div>
-          <div>
-          <ul className="p-4">
-            {projectDetails.map((detail, index) => (
-              <li key={index}>{detail}</li>
-            ))}
-          </ul>
-
+          <div className='flex flex-col justify-center items-center h-full'>
+            <ul className="p-4">
+              {projectDetails.map((detail, index) => (
+                <li key={index}>{detail}</li>
+              ))}
+            </ul>
           </div>
-          
-        </div>
+        </div>  
+        
       ) : (
-        <div className='flex flex-col items-center gap-3 '>
-          <div className='flex justify-between items-center w-full'>
+        <div className='flex flex-col items-center gap-3'>
+          <div className='flex justify-between items-                                                                                     center w-full'>
             <h2 className="text-xl font-bold text-gray-700 hover:text-black">{title}</h2>
             <button onClick={clickHandler} className='p-2 rounded text-white bg-slate-600 hover:bg-slate-500 duration-200 ease-in-out '>More details</button>
           </div>
             <img src={image} alt={title} className="w-full " />
             <p className="text-gray-600 hover:text-black">{description}</p>
         </div>
-        
       )}
     </div>
   );
