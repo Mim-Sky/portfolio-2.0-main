6fe216a378df953fe93e518312fc09d3f6b5e0e5
Mim-Sky <horodyski.michal@gmail.com>
2024-04-23 19:06:55 +0000

    Hero text positioned changed social button colours simplified elements structure in contact section

--- src/components/Templates/ContentGrid.jsx
+++ src/components/Templates/ContentGrid.jsx
@@ -7,8 +7,8 @@ import Facebook from "../Icons/faceBook";
 
 const ContentGrid = ({ title, contentComponent }) => {
   return (
-    <div className="md:h-screen p-5 flex justify-center bg-wave_blue/30">
-      <div className="md:flex md:justify-between md:container items-center gap">
+    <div className="flex justify-center bg-wave_blue/30 ">
+      <div className="h-full md:h-screen p-5 flex flex-col md:flex-row justify-evenly items-center gap-8 md:container">
         <div className="h-96 flex flex-col items-center">
           <h2 className="pb-10 md:pb-24 text-text_dark text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold tracking-wide">
             {title}
