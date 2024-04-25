a9624d73a8b651f5a6b041d02c8f4f45b8af0ed3
Mim-Sky <horodyski.michal@gmail.com>
2024-04-25 13:27:53 +0000

    Project section small visual upgrades. Centered container (added <div> around renderContentComponent() which centered card component )

--- src/components/Templates/ContentLeftTitle.jsx
+++ src/components/Templates/ContentLeftTitle.jsx
@@ -1,12 +1,14 @@
 
 const ContentLeftTitle = ({ title, renderContentComponent = () => null }) => {
   return (
-    <div className="h-screen overflow-scroll p-5 flex justify-center bg-wave_blue/30">
+    <div className="h-screen p-5 flex justify-center  bg-wave_blue/30 overflow-scroll">
       <div className="flex flex-col xl:grid xl:grid-cols-[auto_1fr] gap-4 items-center container">
         <h1 className="p-5 text-text_dark text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold xl:rotate-[-90deg] md:self-auto self-center tracking-wide">
           {title}
         </h1>
+        <div>
         {renderContentComponent()}
+        </div>
       </div>
     </div>
   );
